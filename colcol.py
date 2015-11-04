78fd6ba6883dbda0728a411386e9915f935083ef
0b0bby0 <martin_engqvist@hotmail.com>
2015-11-04 22:32:59 +0100

    Removed wsvg dependency. Added tints, shades, desaturete, saturate, continuum, visualize.

--- colcol.py
+++ colcol.py
@@ -30,10 +30,9 @@
 #
 
 
-#This file mainly deals with color conversions, color transformations, and generating color scales.
+#This file deals with color conversions, color transformations, and generating color scales.
 
 import re
-import wsvg
 import colorsys
 
 def is_rgb(in_col):
@@ -48,21 +47,8 @@ def is_rgb(in_col):
 			return False
 	else:
 		return False
-	
-def rgb_to_hex(rgb):
-	'''
-	Convert RGB colors to hex.
-	Input should be a tuple of integers (R, G, B).
-	Output is a string representing a hex numer. For instance '#FFFFFF'.
-	''' 
-	#make sure input is ok
-	assert is_rgb(rgb) is True, 'Error, %s is not a valid RGB color.' % str(rgb)
-	
-	#make conversion
-	return "#%02x%02x%02x" % rgb
 
-	
-	
+
 def is_hex(in_col):
 	'''
 	Check whether an input string is a valid hex value.
@@ -82,6 +68,35 @@ def is_hex(in_col):
 	else:
 		return True
 
+
+def is_hsl(in_col):
+	'''
+	Check whether an input is a valid HSL color.
+	Return True if it is, otherwise False.
+	'''
+	if len(in_col) == 3 and type(in_col) == tuple:
+		if 0<=in_col[0]<=360 and 0<=in_col[1]<=100 and 0<=in_col[2]<=100:
+			return True
+		else:
+			return False
+	else:
+		return False
+
+
+	
+def rgb_to_hex(rgb):
+	'''
+	Convert RGB colors to hex.
+	Input should be a tuple of integers (R, G, B) where each is between 0 and 255.
+	Output is a string representing a hex numer. For instance '#FFFFFF'.
+	''' 
+	#make sure input is ok
+	assert is_rgb(rgb) is True, 'Error, %s is not a valid RGB color.' % str(rgb)
+	
+	#make conversion
+	return "#%02x%02x%02x" % rgb
+
+	
 	
 def hex_to_rgb(in_col):
 	'''
@@ -97,7 +112,71 @@ def hex_to_rgb(in_col):
 	return tuple([int(in_col[s:s+2], 16) for s in range(0, len(in_col), 2)])
 	
 
-def scale(col1, col2, white_mid=False):
+def rgb_to_hsl(in_col):
+	'''
+	Convert RGB colors to HSL.
+	Input should be a tuple of integers (R, G, B) where each is between 0 and 255.
+	Output is a tuple of integers where h is between 0 and 360 and s and l are between 0 and 100. For instance: (162, 47, 39).
+	'''
+	assert is_rgb(in_col), 'Error, %s is not a valid RGB color.' % str(in_col)
+
+	# Convert each RGB integer to a float between 0 and 1
+	r, g, b = map(lambda x: x/255., in_col) 
+
+	# RGB -> HLS
+	h, l, s = colorsys.rgb_to_hls(r, g, b)
+
+	#convert it back to the appropriate integers
+	h = int(round(360*h))
+	l = int(round(100*l))
+	s = int(round(100*s))
+
+	return (h, s, l) 
+
+
+def hsl_to_rgb(in_col):
+	'''
+	Convert HSL colors to RGB.
+	Input should be a tuple of integers where h is between 0 and 360 and s and l are between 0 and 100. For instance: (162, 47, 39).
+	Output is a tuple of integers (R, G, B) where each is between 0 and 255.
+	'''
+	assert is_hsl(in_col), 'Error, %s is not a valid HSL color.' % str(in_col)
+
+	#assign to variables
+	h, s, l = in_col
+
+	# Convert each HSL integer to a float between 0 and 1
+	h = h/360.0
+	s = s/100.0
+	l = l/100.0
+
+	# RGB -> HLS
+	r, g, b = colorsys.hls_to_rgb(h, l, s)
+
+	#convert it back to the appropriate integers
+	r = int(round(255*r))
+	g = int(round(255*g))
+	b = int(round(255*b))
+
+	return (r, g, b) 	
+
+
+
+def hex_to_hsl():
+	'''
+	Convert a hex color to RGB.
+	Input should be a string. For example '#FFFFFF'.
+	Output is a tuple of ...... . For instance: (h, s, l).
+	'''
+	pass
+
+
+def hsl_to_hex():
+	'''
+	'''
+	pass
+
+def scale(col1, col2, number=101, mode='rgb', white_mid=False):
 	'''
 	Function makes a color scale from 0 to 100 using the supplied colors.
 	The variable white_mid is boolean and determines whether the colors 
@@ -124,16 +203,16 @@ def scale(col1, col2, white_mid=False):
 		R, G, B = col1 
 		for i in range(101):
 			color_dict[i] = rgb_to_hex((R+int(R_diff*i), G+int(G_diff*i), B+int(B_diff*i)))
-			
+		
 	elif white_mid is True:
 		first_half = scale((col1), (255,255,255))
 		for key in range(0, 100, 2):
 			color_dict[key/2] = first_half[key]
-			
+		
 		second_half = scale((255,255,255), col2)
 		for key in range(0, 102, 2):
 			color_dict[50+key/2] = second_half[key]
-		
+
 	return color_dict
 	
 	
@@ -248,6 +327,8 @@ def triadic(in_color):
 	   x x
 		O
 
+	#first color is wrong!
+
 	'''
 	rgb = False
 
@@ -260,6 +341,7 @@ def triadic(in_color):
 
 
 	#add first triadic color
+	#this one is wrong!!
 	color_list = ['#' + color.strip('#')[-2:] + color.strip('#')[:4]]
 
 	#add the input color
@@ -419,42 +501,263 @@ def monochromatic():
 	'''
 	pass
 
-def tints():
+
+def tints(in_col, number=10):
 	'''
-	Returns input color as well as 9 tints.
+	Returns input color as well as tints of that color (lighter colors).
+	number specifies how many new ones to return.
 	'''
-	pass
+	assert (is_rgb(in_col) or is_hex(in_col)), 'Error, the input must be a hex color string or an RGB tuple.'
+	assert type(number) is int, 'Error, the input number must be an integer.'
+	assert (2 <= number and number <= 1000), 'Error, the input number must be between 2 and 1000'
+
+	#check whether input is hex, if it is, make it RGB
+	hex_color = is_hex(in_col) 
+	if hex_color:
+		in_col = hex_to_rgb(in_col)
+
+	#assign to  variables
+	r, g, b = in_col
+
+	# Convert to [0, 1]
+	r, g, b = map(lambda x: x/255., [r, g, b]) 
+
+	# RGB -> HLS
+	hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b) 
+
+	#what is the difference of 100% lightness and the current value	
+	diff = 1.0-lightness 
+	
+	#devide the difference on a step size
+	step = diff/float(number)
+
+	#use that step size to generate the 10 increasing lightness values	
+	lightness_list = [lightness + step*s for s in range(1, number+1)]
+
+	#add the input color to a list, then build the 10 new HSL colors, convert to RGB and save in the same list
+	colors = [in_col]
+	colors.extend([map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(hue, l, saturation)) for l in lightness_list])
+
+	#if the input was hex, convert it back
+	if hex_color:
+		colors = [rgb_to_hex(tuple(s)) for s in colors]
+
+	return colors
 
 
-def shades():
+
+
+def shades(in_col, number=10):
 	'''
-	Returns input color as well as 9 shades.
+	Returns input color as well as shades of that color (darker colors).
+	number specifies how many new ones to return.
 	'''
-	pass
+	assert (is_rgb(in_col) or is_hex(in_col)), 'Error, the input must be a hex color string or an RGB tuple.'
+	assert type(number) is int, 'Error, the input number must be an integer.'
+	assert (2 <= number and number <= 1000), 'Error, the input number must be between 2 and 1000'
+
+	#check whether input is hex, if it is, make it RGB
+	hex_color = is_hex(in_col) 
+	if hex_color:
+		in_col = hex_to_rgb(in_col)
+
+	#assign to  variables
+	r, g, b = in_col
+
+	# Convert to [0, 1]
+	r, g, b = map(lambda x: x/255., [r, g, b]) 
+
+	# RGB -> HLS
+	hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b) 
+	
+	#devide the difference on a step size
+	step = lightness/float(number)
+
+	#use that step size to generate the 10 increasing lightness values	
+	lightness_list = [lightness - step*s for s in range(1, number+1)]
+
+	#add the input color to a list, then build the 10 new HSL colors, convert to RGB and save in the same list
+	colors = [in_col]
+	colors.extend([map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(hue, l, saturation)) for l in lightness_list])
+
+	#if the input was hex, convert it back
+	if hex_color:
+		colors = [rgb_to_hex(tuple(s)) for s in colors]
+
+	return colors
 
 
-def saturate():
+def saturate(in_col, number=10):
 	'''
-	Returns the input color as well as 9 more saturated versions of that color.
+	Returns the input color as well as more saturated versions of that color.
+	number specifies how many new ones to return.
 	'''
-	pass
+	assert (is_rgb(in_col) or is_hex(in_col)), 'Error, the input must be a hex color string or an RGB tuple.'
+	assert type(number) is int, 'Error, the input number must be an integer.'
+	assert (2 <= number and number <= 1000), 'Error, the input number must be between 2 and 1000'
+
+	#check whether input is hex, if it is, make it RGB
+	hex_color = is_hex(in_col) 
+	if hex_color:
+		in_col = hex_to_rgb(in_col)
+
+	#assign to  variables
+	r, g, b = in_col
+
+	# Convert to [0, 1]
+	r, g, b = map(lambda x: x/255., [r, g, b]) 
+
+	# RGB -> HLS
+	hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b) 
+
+	#what is the difference of 100% saturation and the current value	
+	diff = 1.0-saturation 
+	
+	#devide the difference on a step size
+	step = diff/float(number)
+
+	#use that step size to generate the 10 increasing saturation values	
+	saturation_list = [saturation + step*s for s in range(1, number+1)]
+
+	#add the input color to a list, then build the 10 new HSL colors, convert to RGB and save in the same list
+	colors = [in_col]
+	colors.extend([map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(hue, lightness, s)) for s in saturation_list])
+
+	#if the input was hex, convert it back
+	if hex_color:
+		colors = [rgb_to_hex(tuple(s)) for s in colors]
+
+	return colors
 
 
-def desaturate():
+def desaturate(in_col, number=10):
 	'''
-	Returns the input color as well as 9 less saturated versions of that color.
+	Returns the input color as well as less saturated versions of that color.
+	number specifies how many new ones to return.
 	'''
-	pass
+	assert (is_rgb(in_col) or is_hex(in_col)), 'Error, the input must be a hex color string or an RGB tuple.'
+	assert type(number) is int, 'Error, the input number must be an integer.'
+	assert (2 <= number and number <= 1000), 'Error, the input number must be between 2 and 1000'
+
+	#check whether input is hex, if it is, make it RGB
+	hex_color = is_hex(in_col) 
+	if hex_color:
+		in_col = hex_to_rgb(in_col)
+
+	#assign to  variables
+	r, g, b = in_col
+
+	# Convert to [0, 1]
+	r, g, b = map(lambda x: x/255., [r, g, b]) 
+
+	# RGB -> HLS
+	hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b) 
+	
+	#devide the difference on a step size
+	step = saturation/float(number)
+
+	#use that step size to generate the 10 increasing saturation values	
+	saturation_list = [saturation - step*s for s in range(1, number+1)]
+
+	#add the input color to a list, then build the 10 new HSL colors, convert to RGB and save in the same list
+	colors = [in_col]
+	colors.extend([map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(hue, lightness, s)) for s in saturation_list])
+
+	#if the input was hex, convert it back
+	if hex_color:
+		colors = [rgb_to_hex(tuple(s)) for s in colors]
+
+	return colors
 
 
-
-
-
-def visualize():
+def continuum(in_col, number=10):
 	'''
-	Takes a list of colors and visualizes them.
+	Returns the entire color wheel starting with the input color.
+	number specifies how many new ones to return.
 	'''
-	raise NotImplementedError
+	assert (is_rgb(in_col) or is_hex(in_col)), 'Error, the input must be a hex color string or an RGB tuple.'
+	assert type(number) is int, 'Error, the input number must be an integer.'
+	assert (2 <= number and number <= 1000), 'Error, the input number must be between 2 and 1000'
+
+	#check whether input is hex, if it is, make it RGB
+	hex_color = is_hex(in_col) 
+	if hex_color:
+		in_col = hex_to_rgb(in_col)
+
+	#assign to  variables
+	r, g, b = in_col
+
+	# Convert to [0, 1]
+	r, g, b = map(lambda x: x/255., [r, g, b]) 
+
+	# RGB -> HLS
+	hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b) 
+	
+	# Rotation by defined number of degrees
+	angle = (360/float(number))/360.0
+	h_list = [(hue+ang) % 1 for ang in [angle*s for s in range(number)]]
+
+	colors = [map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(h, lightness, saturation)) for h in h_list] # H'LS -> new RGB
+	colors = [tuple(s) for s in colors]
+
+	#if the input was hex, convert it back
+	if hex_color:
+		colors = [rgb_to_hex(s) for s in colors]
+
+	return colors
+
+
+def visualize(color_list=['#acc123','#ffffff','#000000', '#1ccf9c']):
+	'''
+	Visualizes a list of colors.
+	Useful to see what one gets out of the different functions.
+	'''
+
+	#need to adapt box size depending on how many colors are in list
+
+	#asserts.... here....
+
+
+	from Tkinter import Tk, Canvas, Frame, BOTH
+
+	#check whether input is hex, if it is, make it RGB
+	rgb_color = is_rgb(color_list[0]) 
+	if rgb_color:
+		color_list = [rgb_to_hex(s) for s in color_list]
+	 
+
+	class Example(Frame):
+	  
+		def __init__(self, parent, cl):
+			Frame.__init__(self, parent)   
+
+			self.parent = parent   
+			self.color_list = cl     
+			self.initUI()
+		    
+		def initUI(self):
+
+			self.parent.title("Colors")        
+			self.pack(fill=BOTH, expand=1)
+
+			canvas = Canvas(self)
+
+			#modify rectangle size based on how many colors there are
+			rect_size = 700/float(len(color_list))
+
+			for i in range(len(self.color_list)):
+				canvas.create_rectangle(10+rect_size*i, 10, 10+rect_size*(i+1), 110, outline=self.color_list[i], fill=self.color_list[i])
+			canvas.pack(fill=BOTH, expand=1)
+
+
+	def main():
+
+		root = Tk()
+		ex = Example(root, color_list)
+		root.geometry("720x120+250+300")
+		root.mainloop()  
+
+	main() 
 
 
 def NextRGB(color = (0,0,0)):
@@ -483,86 +786,41 @@ def NextRGB(color = (0,0,0)):
 	
 	
 	
-def test_scale():
-	'''
-	For testing the color scale, make an svg file.
-	'''
-	
-	name = 'testing'
-	scene = wsvg.Scene(name=name, size=(400, 400))
+#good scales
 
-	def paintit(x, y, col1, col2, white_mid):
-		col_dict = scale(col1, col2, white_mid)
-		for n in range(0, 101):
-			y += 0.5
-			scene.add(wsvg.Rectangle(origin=(x, y), height=0.5, width=10, fill_color=col_dict[n], line_color=col_dict[n], line_width=0.5))	
+#purple to tiel
+#col1 = '#800080'
+#col2 = '#008080'
 
-	#purple to tiel
-	col1 = '#800080'
-	col2 = '#008080'
-	x = 10
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	
-	#orange to blue
-	col1 = '#ffc500'
-	col2 = '#056efa'
-	x = 30
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	
-	#blue to red
-	col1 = '#817cbb'
-	col2 = '#c12133'
-	x = 50
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()
-	
-	#another blue to red
-	col1 = '#30acdf'
-	col2 = '#ef292b'
-	x = 70
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()	
+#orange to blue
+#col1 = '#ffc500'
+#col2 = '#056efa'
 
-	#orange to green
-	col1 = '#ff6600'
-	col2 = '#2c9082'
-	x = 90
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()		
-	
-	#orange to dark blue
-	col1 = '#ff6600'
-	col2 = '#18567d'
-	x = 110
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()	
-	
-	#grey to orange
-	col1 = '#666666'
-	col2 = '#ff6600'
-	x = 130
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()	
+#blue to red
+#col1 = '#817cbb'
+#col2 = '#c12133'
 
-	#dark yellow to blue
-	col1 = '#cba916'
-	col2 = '#8eb6d5'
-	x = 130
-	y = 10
-	paintit(x, y, col1, col2, white_mid=True)
-	scene.write_svg()	
+#another blue to red
+#col1 = '#30acdf'
+#col2 = '#ef292b'
+
+#orange to green
+#col1 = '#ff6600'
+#col2 = '#2c9082'
+
+#orange to dark blue
+#col1 = '#ff6600'
+#col2 = '#18567d'
+
+#grey to orange
+#col1 = '#666666'
+#col2 = '#ff6600'
+
+#dark yellow to blue
+#col1 = '#cba916'
+#col2 = '#8eb6d5'
 
 	
-if __name__ == "__main__": 
-#	test_scale()
-	print(triadic('#ffa2b5'))
-	print(triadic((255, 162, 181)))
+
 	 	
 	
